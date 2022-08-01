278281228d4dea4a3957cb53c92ec4f753ee7f14
Abolfazl Soltani <abolfazlsoltaninia8@gmail.com>
2022-08-01 10:24:53 +0430

    comment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  ReactDom from  'react-dom';
+import CommentDetails from './CommentDetails';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+    return (
+     <div className="ui comments container">
+        <CommentDetails  author="Ali"  date="1 days ago" comment="it was really good"/>
+         <CommentDetails  author="mohammad"  date="30 days ago" comment="it was really usfull"/>
+         <CommentDetails author="Abolfazlsoltaninia"  date="29 days ago" comment="it was really bad"/>
+     </div>
+    );
+};
+const root = document.getElementById('root');
+ReactDom.render(<App/>, root);
